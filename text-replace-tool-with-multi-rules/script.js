5eb07928ca690b533d704fe15baaa9e6c2ec2ea4
linyuxuanlin <824676271@qq.com>
2023-08-22 22:02:37 +0800

    1

--- text-replace-tool-with-multi-rules/script.js
+++ text-replace-tool-with-multi-rules/script.js
@@ -1,67 +1,71 @@
 function addReplaceRule() {
-    const replaceRulesDiv = document.getElementById("replaceRules");
-    const newReplaceRuleDiv = document.createElement("div");
-    const ruleIndex = replaceRulesDiv.children.length + 1;
+  const replaceRulesDiv = document.getElementById("replaceRules");
+  const newReplaceRuleDiv = document.createElement("div");
+  const ruleIndex = replaceRulesDiv.children.length + 1;
 
-    newReplaceRuleDiv.className = "replaceRule";
-    newReplaceRuleDiv.innerHTML = `
+  newReplaceRuleDiv.className = "replaceRule";
+  newReplaceRuleDiv.innerHTML = `
 <label for="find${ruleIndex}">待替换文本 ${ruleIndex}:</label>
 <input type="text" class="find" id="find${ruleIndex}" oninput="applyReplacements()">
 <label for="replace${ruleIndex}">替换后文本 ${ruleIndex}:</label>
 <input type="text" class="replace" id="replace${ruleIndex}" oninput="applyReplacements()">
 `;
 
-    replaceRulesDiv.appendChild(newReplaceRuleDiv);
-  }
+  replaceRulesDiv.appendChild(newReplaceRuleDiv);
+}
 
-  function applyReplacements() {
-    const inputText = document.getElementById("input").value;
-    const replaceRules = document.querySelectorAll(".replaceRule");
+function applyReplacements() {
+  const inputText = document.getElementById("input").value;
+  const replaceRules = document.querySelectorAll(".replaceRule");
 
-    let outputText = inputText;
+  let outputText = inputText;
 
-    replaceRules.forEach((rule, index) => {
-      const findInput = rule.querySelector(`#find${index + 1}`);
-      const replaceInput = rule.querySelector(`#replace${index + 1}`);
+  replaceRules.forEach((rule, index) => {
+    const findInput = rule.querySelector(`#find${index + 1}`);
+    const replaceInput = rule.querySelector(`#replace${index + 1}`);
 
-      if (findInput && replaceInput) {
-        const find = findInput.value;
-        let replace = replaceInput.value;
+    if (findInput && replaceInput) {
+      const find = findInput.value;
+      let replace = replaceInput.value;
 
-        if (find !== "" && replace !== "") {
-          const regex = new RegExp(find, "g");
+      if (find !== "" && replace !== "") {
+        const regex = new RegExp(find, "g");
 
-          // Escape ${replace} with an HTML entity
-          replace = replace.replace(/\$\{replace\}/g, "&#36;{replace}");
+        // Escape ${replace} with an HTML entity
+        replace = replace.replace(/\$\{replace\}/g, "&#36;{replace}");
 
-          outputText = outputText.replace(regex, (match) => {
-            // Check if the match is surrounded by HTML tags
-            if (match.startsWith("<") && match.endsWith(">")) {
-              return `<span class="highlight">${replace}</span>`;
-            } else {
-              return match.replace(
-                find,
-                `<span class="highlight">${replace}</span>`
-              );
-            }
-          });
-        }
+        outputText = outputText.replace(regex, (match) => {
+          // Check if the match is surrounded by HTML tags
+          if (match.startsWith("<") && match.endsWith(">")) {
+            return `<span class="highlight">${replace}</span>`;
+          } else {
+            return match.replace(
+              find,
+              `<span class="highlight">${replace}</span>`
+            );
+          }
+        });
       }
-    });
+    }
+  });
 
-    // Unescape HTML entities back to ${replace}
-    outputText = outputText.replace(/&#36;/g, "$");
+  // Unescape HTML entities back to ${replace}
+  outputText = outputText.replace(/&#36;/g, "$");
 
-    document.getElementById("output").innerHTML = outputText;
-  }
+  document.getElementById("output").innerHTML = outputText;
+}
 
-  function copyOutput() {
-    const outputText = document.getElementById("output").innerText;
-    const tempTextArea = document.createElement("textarea");
-    tempTextArea.value = outputText;
-    document.body.appendChild(tempTextArea);
-    tempTextArea.select();
-    document.execCommand("copy");
-    document.body.removeChild(tempTextArea);
-    alert("已复制到剪贴板！");
-  }
+function copyOutput() {
+  const outputText = document.getElementById("output").innerText;
+  const tempTextArea = document.createElement("textarea");
+  tempTextArea.value = outputText;
+  document.body.appendChild(tempTextArea);
+  tempTextArea.select();
+  document.execCommand("copy");
+  document.body.removeChild(tempTextArea);
+  alert("已复制到剪贴板！");
+}
+
+function goToHomePage() {
+  window.location.href = "../index.html";
+}
