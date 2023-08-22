7691d5c068a5fcf711f97de35d17afc907a79202
linyuxuanlin <824676271@qq.com>
2023-08-22 21:56:04 +0800

    4

--- text-replace-tool-with-multi-rules/script.js
+++ text-replace-tool-with-multi-rules/script.js
@@ -2,42 +2,59 @@ function addReplaceRule() {
     const replaceRulesDiv = document.getElementById("replaceRules");
     const newReplaceRuleDiv = document.createElement("div");
     const ruleIndex = replaceRulesDiv.children.length + 1;
-  
+
     newReplaceRuleDiv.className = "replaceRule";
     newReplaceRuleDiv.innerHTML = `
-      <label for="find${ruleIndex}">待替换文本 ${ruleIndex}:</label>
-      <input type="text" class="find" id="find${ruleIndex}" oninput="applyReplacements()">
-      <label for="replace${ruleIndex}">替换后文本 ${ruleIndex}:</label>
-      <input type="text" class="replace" id="replace${ruleIndex}" oninput="applyReplacements()">
-    `;
-  
+<label for="find${ruleIndex}">待替换文本 ${ruleIndex}:</label>
+<input type="text" class="find" id="find${ruleIndex}" oninput="applyReplacements()">
+<label for="replace${ruleIndex}">替换后文本 ${ruleIndex}:</label>
+<input type="text" class="replace" id="replace${ruleIndex}" oninput="applyReplacements()">
+`;
+
     replaceRulesDiv.appendChild(newReplaceRuleDiv);
   }
-  
+
   function applyReplacements() {
     const inputText = document.getElementById("input").value;
     const replaceRules = document.querySelectorAll(".replaceRule");
-  
+
     let outputText = inputText;
-  
+
     replaceRules.forEach((rule, index) => {
       const findInput = rule.querySelector(`#find${index + 1}`);
       const replaceInput = rule.querySelector(`#replace${index + 1}`);
-  
+
       if (findInput && replaceInput) {
         const find = findInput.value;
-        const replace = replaceInput.value;
-  
+        let replace = replaceInput.value;
+
         if (find !== "" && replace !== "") {
           const regex = new RegExp(find, "g");
-          outputText = outputText.replace(regex, match => `<span class="highlight">${replace}</span>`);
+
+          // Escape ${replace} with an HTML entity
+          replace = replace.replace(/\$\{replace\}/g, "&#36;{replace}");
+
+          outputText = outputText.replace(regex, (match) => {
+            // Check if the match is surrounded by HTML tags
+            if (match.startsWith("<") && match.endsWith(">")) {
+              return `<span class="highlight">${replace}</span>`;
+            } else {
+              return match.replace(
+                find,
+                `<span class="highlight">${replace}</span>`
+              );
+            }
+          });
         }
       }
     });
-  
+
+    // Unescape HTML entities back to ${replace}
+    outputText = outputText.replace(/&#36;/g, "$");
+
     document.getElementById("output").innerHTML = outputText;
   }
-  
+
   function copyOutput() {
     const outputText = document.getElementById("output").innerText;
     const tempTextArea = document.createElement("textarea");
@@ -48,53 +65,3 @@ function addReplaceRule() {
     document.body.removeChild(tempTextArea);
     alert("已复制到剪贴板！");
   }
-  
-  function exportRules() {
-    const replaceRules = document.querySelectorAll(".replaceRule");
-    const rules = [];
-  
-    replaceRules.forEach((rule, index) => {
-      const findInput = rule.querySelector(`#find${index + 1}`);
-      const replaceInput = rule.querySelector(`#replace${index + 1}`);
-  
-      if (findInput && replaceInput) {
-        const find = findInput.value;
-        const replace = replaceInput.value;
-        rules.push(`${find},${replace}`);
-      }
-    });
-  
-    const rulesText = rules.join(";");
-    const blob = new Blob([rulesText], { type: "text/plain;charset=utf-8" });
-    saveAs(blob, "replace_rules.txt");
-  }
-  
-  function importRules(event) {
-    const file = event.target.files[0];
-    if (file) {
-      const reader = new FileReader();
-  
-      reader.onload = function(e) {
-        const rulesText = e.target.result;
-        const rulesArray = rulesText.split(";").map(rule => rule.trim()).filter(rule => rule !== "");
-  
-        rulesArray.forEach((rule, index) => {
-          const [find, replace] = rule.split(",");
-          const ruleIndex = index + 1;
-  
-          const findInput = document.querySelector(`#find${ruleIndex}`);
-          const replaceInput = document.querySelector(`#replace${ruleIndex}`);
-  
-          if (findInput && replaceInput) {
-            findInput.value = find;
-            replaceInput.value = replace;
-          }
-        });
-  
-        applyReplacements();
-      };
-  
-      reader.readAsText(file);
-    }
-  }
-  
