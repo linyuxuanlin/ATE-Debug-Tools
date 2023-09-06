7e5a0eb07333e46315c986f3158b6d9a5ad7447c
linyuxuanlin <824676271@qq.com>
2023-09-06 14:59:27 +0800

    1

---
+++ radix-converter/script.js
@@ -0,0 +1,105 @@
+// 获取 DOM 元素
+const inputBaseRadios = document.getElementsByName("input-base");
+const inputElement = document.getElementById("input");
+const outputBaseRadios = document.getElementsByName("output-base");
+const outputElement = document.getElementById("output");
+const switchBtn = document.getElementById("switch-btn");
+
+// 转换函数
+function convert() {
+  let inputBase, outputBase;
+  for (let i = 0; i < inputBaseRadios.length; i++) {
+    if (inputBaseRadios[i].checked) {
+      inputBase = parseInt(inputBaseRadios[i].value);
+      break;
+    }
+  }
+  for (let i = 0; i < outputBaseRadios.length; i++) {
+    if (outputBaseRadios[i].checked) {
+      outputBase = parseInt(outputBaseRadios[i].value);
+      break;
+    }
+  }
+  const input = inputElement.value;
+
+  // 将输入内容转换为十进制
+  const decimal = input ? parseInt(input, inputBase) : NaN;
+
+  // 将十进制转换为输出进制
+  const output = isNaN(decimal) ? "" : decimal.toString(outputBase);
+
+  // 显示结果
+  outputElement.textContent = output;
+}
+
+// 自动转换输入
+inputElement.addEventListener("input", convert);
+
+// 一键切换函数
+function switchOutput() {
+  // 获取当前输入和输出的值和进制
+  const input = inputElement.value;
+  const output = outputElement.textContent;
+  let inputBase, outputBase;
+  for (let i = 0; i < inputBaseRadios.length; i++) {
+    if (inputBaseRadios[i].checked) {
+      inputBase = parseInt(inputBaseRadios[i].value);
+      break;
+    }
+  }
+  for (let i = 0; i < outputBaseRadios.length; i++) {
+    if (outputBaseRadios[i].checked) {
+      outputBase = parseInt(outputBaseRadios[i].value);
+      break;
+    }
+  }
+
+  // 切换输入和输出的值和进制
+  inputElement.value = output;
+  outputElement.textContent = input;
+  for (let i = 0; i < inputBaseRadios.length; i++) {
+    if (parseInt(inputBaseRadios[i].value) === outputBase) {
+      inputBaseRadios[i].checked = true;
+      break;
+    }
+  }
+  for (let i = 0; i < outputBaseRadios.length; i++) {
+    if (parseInt(outputBaseRadios[i].value) === inputBase) {
+      outputBaseRadios[i].checked = true;
+      break;
+    }
+  }
+
+  // 自动转换输出
+  convert();
+}
+
+// 双击输出结果复制
+outputElement.addEventListener("dblclick", () => {
+  navigator.clipboard.writeText(outputElement.textContent);
+  alert("已复制");
+});
+
+// 一键复制函数
+function copyOutput() {
+  // 复制结果到剪贴板
+  navigator.clipboard.writeText(outputElement.textContent);
+}
+
+// 绑定事件
+switchBtn.addEventListener("click", switchOutput);
+
+function copyOutput() {
+  const outputText = document.getElementById("output").innerText;
+  const tempTextArea = document.createElement("textarea");
+  tempTextArea.value = outputText;
+  document.body.appendChild(tempTextArea);
+  tempTextArea.select();
+  document.execCommand("copy");
+  document.body.removeChild(tempTextArea);
+  alert("已复制到剪贴板！");
+}
+
+function goToHomePage() {
+  window.location.href = "../index.html";
+}
